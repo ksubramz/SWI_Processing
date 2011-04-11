a6695a2f38685de29d134110c376cbd99c873a3a
Krish Subramaniam <krish@hima.nmr.mgh.harvard.edu>
2011-04-11 15:51:14 -0400

    ENH: phase mask methods

--- swi_process.cpp
+++ swi_process.cpp
@@ -52,13 +52,168 @@ struct IoParams
   std::string phasefile;
   std::string swioutput;
   float stddev;
+  std::string phasemethod;
   int phasemult;
   float phasecutoff;
+  float phasercutoff;
+  float sigmoid_a;
+  float sigmoid_b;
   int miplevel;
 
   IoParams();
   void parse(int ac, char* av[]);
 };
+
+void do_phase_mask(IoParams& params, MRI* mag, MRI* pha, MRI* out)
+{  
+
+  float mval, pval, outval, _ov =0.0, v;
+  
+  if (params.phasemethod.compare("negative") == 0) 
+  {
+    std::cout << "Performing phase mask multiplication with cutoff " << params.phasecutoff << " ..\n";
+    std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
+    for ( int i=0; i < out->width; i++)
+      for ( int j=0; j < out->height; j++)
+        for ( int k=0; k < out->depth; k++)
+        {
+          mval = MRIgetVoxVal(mag, i, j, k, 0);
+          pval = MRIgetVoxVal(pha, i, j, k, 0);
+          // do phase cut off
+          if ( pval > 0.0 )
+            _ov = 1.0;
+          else if ( pval <= params.phasecutoff )
+            _ov = 0.0;
+          else
+            _ov = 1.0 - (pval / params.phasecutoff );
+
+          // do phase multiplications
+          outval=1.0;
+          for (int phm=0; phm < params.phasemult; phm++) 
+            outval = outval * _ov;
+          v = outval * mval;
+          MRIsetVoxVal(out, i, j, k, 0, v);
+        }
+  }
+
+  if (params.phasemethod.compare("positive") == 0) 
+  {
+    std::cout << "Performing phase mask multiplication with right cutoff " << params.phasercutoff << " ..\n";
+    std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
+    for ( int i=0; i < out->width; i++)
+      for ( int j=0; j < out->height; j++)
+        for ( int k=0; k < out->depth; k++)
+        {
+          mval = MRIgetVoxVal(mag, i, j, k, 0);
+          pval = MRIgetVoxVal(pha, i, j, k, 0);
+          // do phase cut off
+          if ( pval < 0.0 )
+            _ov = 1.0;
+          else if ( pval >= params.phasercutoff )
+            _ov = 0.0;
+          else
+            _ov =  1.0 - (pval / params.phasercutoff ) ;
+
+          // do phase multiplications
+          outval=1.0;
+          for (int phm=0; phm < params.phasemult; phm++) 
+            outval = outval * _ov;
+          v = outval * mval;
+          MRIsetVoxVal(out, i, j, k, 0, v);
+        }
+  }
+
+  if (params.phasemethod.compare("symmetric") == 0) 
+  {
+    std::cout << "Performing phase mask multiplication with cutoff " << params.phasecutoff << " ..\n";
+    std::cout << "Performing phase mask multiplication with right cutoff " << params.phasercutoff << " ..\n";
+    std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
+    if ( fabs( fabs(params.phasecutoff) - fabs(params.phasercutoff)) < 0.0001 ) 
+        std::cerr << "ERROR: symmetric option implies both left cutoff and right cutoff are almost same\n";
+    for ( int i=0; i < out->width; i++)
+      for ( int j=0; j < out->height; j++)
+        for ( int k=0; k < out->depth; k++)
+        {
+          mval = MRIgetVoxVal(mag, i, j, k, 0);
+          pval = MRIgetVoxVal(pha, i, j, k, 0);
+          // do phase cut off
+          if ( pval < params.phasecutoff )
+            _ov = 0.0;
+          else if ( pval >= params.phasercutoff )
+            _ov = 0.0;
+          else if ( pval >= 0.0 ) 
+            _ov =  1.0 - (pval / params.phasercutoff ) ;
+          else
+            _ov =  1.0 - (pval / params.phasecutoff ) ;
+
+          // do phase multiplications
+          outval=1.0;
+          for (int phm=0; phm < params.phasemult; phm++) 
+            outval = outval * _ov;
+          v = outval * mval;
+          MRIsetVoxVal(out, i, j, k, 0, v);
+        }
+  }
+
+  if (params.phasemethod.compare("asymmetric") == 0) 
+  {
+    std::cout << "Performing phase mask multiplication with cutoff " << params.phasecutoff << " ..\n";
+    std::cout << "Performing phase mask multiplication with right cutoff " << params.phasercutoff << " ..\n";
+    std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
+    for ( int i=0; i < out->width; i++)
+      for ( int j=0; j < out->height; j++)
+        for ( int k=0; k < out->depth; k++)
+        {
+          mval = MRIgetVoxVal(mag, i, j, k, 0);
+          pval = MRIgetVoxVal(pha, i, j, k, 0);
+          // do phase cut off
+          if ( pval < params.phasecutoff )
+            _ov = 0.0;
+          else if ( pval >= params.phasercutoff )
+            _ov = 1.0;
+          else if ( pval >= 0.0 ) 
+            _ov =  1.0 - (pval / params.phasercutoff ) ;
+          else
+            _ov =  1.0 - (pval / params.phasecutoff ) ;
+
+          // do phase multiplications
+          outval=1.0;
+          for (int phm=0; phm < params.phasemult; phm++) 
+            outval = outval * _ov;
+          v = outval * mval;
+          MRIsetVoxVal(out, i, j, k, 0, v);
+        }
+  }
+  
+  if (params.phasemethod.compare("sigmoid") == 0) 
+  {
+    std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
+    float a = params.sigmoid_a;
+    float b = params.sigmoid_b;
+    for ( int i=0; i < out->width; i++)
+      for ( int j=0; j < out->height; j++)
+        for ( int k=0; k < out->depth; k++)
+        {
+          mval = MRIgetVoxVal(mag, i, j, k, 0);
+          pval = MRIgetVoxVal(pha, i, j, k, 0);
+          if ( pval < params.phasecutoff )
+            _ov = 0.0;
+          else if ( pval >= params.phasercutoff )
+            _ov = 1.0;
+          else 
+            _ov = 1.0 / ( 1.0 + exp (-a * ( pval - b ) ) );
+
+          // do phase multiplications
+          outval=1.0;
+          for (int phm=0; phm < params.phasemult; phm++) 
+            outval = outval * _ov;
+          v = outval * mval;
+          MRIsetVoxVal(out, i, j, k, 0, v);
+        }
+  }
+
+
+}
 
 int main(int argc, char*argv[])
 {
@@ -91,7 +246,15 @@ int main(int argc, char*argv[])
     exit(1);
   }
 
-  float mval, pval, outval, _ov =0.0, v;
+  if  ((params.phasemethod.compare("negative") != 0 ) &&
+      (params.phasemethod.compare("positive") != 0 ) &&
+      (params.phasemethod.compare("symmetric") != 0 ) &&
+      (params.phasemethod.compare("asymmetric") != 0 ) &&
+      (params.phasemethod.compare("sigmoid") != 0 ))
+      {
+        std::cerr << "ERROR: The phase mask method has to be one of negative, positive, symmetric, asymmetric or sigmoid\n";
+      }
+
   float vmin, vmax;
   MRI *outimg = MRIclone(mriphase, NULL);
   // the following are the various intermediary images
@@ -109,32 +272,14 @@ int main(int argc, char*argv[])
   //
   MRIvalRange(_tmpimg, &vmin, &vmax);
   std::cout << "Min and max of the highpass image " << vmin << " " << vmax << "\n";
-
+  
   if ( params.phasecutoff < vmin )
     params.phasecutoff = vmin;
-  std::cout << "Performing phase mask multiplication with cutoff " << params.phasecutoff << " ..\n";
-  std::cout << "Number of phase multiplications " << params.phasemult << " ..\n";
-  for ( int i=0; i < _hpimg->width; i++)
-    for ( int j=0; j < _hpimg->height; j++)
-      for ( int k=0; k < _hpimg->depth; k++)
-      {
-        mval = MRIgetVoxVal(mrimag, i, j, k, 0);
-        pval = MRIgetVoxVal(_tmpimg, i, j, k, 0);
-        // do phase cut off
-        if ( pval > 0.0 )
-          _ov = 1.0;
-        else if ( pval <= params.phasecutoff )
-          _ov = 0.0;
-        else
-          _ov = 1.0 - (pval / params.phasecutoff );
-        
-        // do phase multiplications
-        outval=1.0;
-        for (int phm=0; phm < params.phasemult; phm++) 
-          outval = outval * _ov;
-        v = outval * mval;
-        MRIsetVoxVal(_hpimg, i, j, k, 0, v);
-      }
+  if (params.phasercutoff > vmax )
+    params.phasercutoff = vmax;
+
+  // perform phase masking operation. Result is in _hpimg
+  do_phase_mask(params, mrimag, _tmpimg, _hpimg);
 
   // minimum Intensity Projection along y
   std::cout << "Performing Minimum Intensity Projection along y direction with levels=" << params.miplevel << " ..\n";
@@ -174,7 +319,11 @@ int main(int argc, char*argv[])
 IoParams::IoParams()
 {
   stddev      = 2.0;
+  phasemethod = "negative";
   phasecutoff = -std::numeric_limits<float>::max();
+  phasercutoff = std::numeric_limits<float>::max();
+  sigmoid_a   = 1.0;
+  sigmoid_b   = 0.0;
   phasemult   = 4;
   miplevel    = 4;
 }
@@ -187,8 +336,12 @@ void IoParams::parse(int argc, char* argv[])
   cmd.AddOptionString("phase_file", &phasefile, "The phase image ( Output from the PRELUDE program)");
   cmd.AddOptionString("swi_output", &swioutput, "Name of the SWI processed output image");
   cmd.AddOptionFloat("stddev", &stddev, "Specify the standard deviation of the Gaussian Smoothing Filter. Default is 2.0");
+  cmd.AddOptionString("phase_mask_method", &phasemethod, "Specify the phase mask method. One of negative, positive, symmetric, asymmetric, sigmoid. Default is negative");
   cmd.AddOptionInt("phase_multiplications", &phasemult, "Specify the number of phase multiplications. Default is 4");
   cmd.AddOptionFloat("phase_mask_cutoff", &phasecutoff, "Specify the negative phase mask cutoff frequency ( in radianѕ). Default is the minimum value of the phase image.");
+  cmd.AddOptionFloat("phase_mask_right_cutoff", &phasercutoff, "Specify the positive phase mask cutoff frequency ( in radianѕ). Default is the maximum value of the phase image.");
+  cmd.AddOptionFloat("sigmoid_a", &sigmoid_a, "Specify 'a' for the sigmoid formula f(phase)=1/(1+exp(-a*(phase-b))). Default is 1.0. Meaningless with phase_method != sigmoid");
+  cmd.AddOptionFloat("sigmoid_b", &sigmoid_b, "Specify 'b' for the sigmoid formula f(phase)=1/(1+exp(-a*(phase-b))). Default is 0.0. Meaningless with phase_method != sigmoid");
   cmd.AddOptionInt("mip_level", &miplevel, "Specify the number of levels of mIP across the y direction. Default is 4");
   if ( argc == 1 )
   {
